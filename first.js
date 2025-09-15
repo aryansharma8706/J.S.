940bf3caa3c83817166fdbdb993325c989576be2
aryansharma8706 <kailashchandrasharma844@gmail.com>
2025-09-15 21:36:25 +0530

    variables

--- first.js
+++ first.js
@@ -1,6 +1,18 @@
 console.log("Hello world");
-function sum(a, b) {
-    return a + b;
-}
+var  a = 56;
+console.log(a);
+a = 'Ajay';
+console.log(a);
 
-console.log(sum(2, 3));
+// var,let,and const difference
+// var a = 45;
+let b = 'Harsh';
+ const author = 'Aakash'//const ko vapas declared nhi kr skte hai error dega
+//  author = 56;
+let d= undefined;
+let c = null;
+{
+    let b = 'This';
+    console.log(b);
+}
+console.log(b);
