1ae713fddfaf2444a582638702d3f7ae5068d1ab
aryansharma8706 <kailashchandrasharma844@gmail.com>
2025-09-17 21:04:17 +0530

    if-else-if

--- condtional.js
+++ condtional.js
@@ -1,5 +1,15 @@
 let a = prompt("enter your age ")
 a = Number.parseInt(a)
-if(a>0){
-    alert("This is a valid age")
+if(a<0){
+     alert("This is an invalid age")
 }
+else if(a<9){
+     alert("You are a kid and you cannot even think of driving")
+}
+else if(a<18 && a>=9){
+     alert("You are a kid and you can think of driving after 18")
+}
+else{
+    alert("You can now drive")
+}
+console.log("Done")
