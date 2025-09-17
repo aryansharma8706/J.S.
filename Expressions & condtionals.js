82182957816a66d0a8105f163a224637bcee581d
aryansharma8706 <kailashchandrasharma844@gmail.com>
2025-09-17 20:51:39 +0530

    prompt

--- Expressions & condtionals.js
+++ Expressions & condtionals.js
@@ -1,6 +1,7 @@
 // 77;
 // "Aryan sharma";
 // false;
+// *Arithematic Operator
 console.log("Operators in js ")
 let a = 10;
 let b= 4;
@@ -15,3 +16,22 @@ console.log("--a =",--a)
 console.log("a-- =",a--)
 console.log("a =",a)
 console.log("a-- =",a--)
+
+//* Assignment operator
+b = 1;
+b += 5;
+console.log("b =", b)
+b -= 5
+console.log("b = ",b)
+b *= 5
+console.log("b = ",b)
+b /= 5
+console.log("b = ",b)
+
+// *comparison operator
+let comp1 = 6;
+let comp2 = 7;
+console.log("comp1 == comp2",comp1 == comp2);
+console.log("comp1 != comp2",comp1 != comp2);
+console.log("comp1 === comp2",comp1 === comp2);
+console.log("comp1 !== comp2",comp1 !== comp2);
---
+++ condtional.js
@@ -0,0 +1,5 @@
+let a = prompt("enter your age ")
+a = Number.parseInt(a)
+if(a>0){
+    alert("This is a valid age")
+}
