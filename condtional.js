39c73736a6b32cbde4aafada19bc2081e1e6935f
aryansharma8706 <kailashchandrasharma844@gmail.com>
2025-09-17 21:14:15 +0530

    ternary operator

--- condtional.js
+++ condtional.js
@@ -1,15 +1,17 @@
 let a = prompt("enter your age ")
 a = Number.parseInt(a)
-if(a<0){
-     alert("This is an invalid age")
-}
-else if(a<9){
-     alert("You are a kid and you cannot even think of driving")
-}
-else if(a<18 && a>=9){
-     alert("You are a kid and you can think of driving after 18")
-}
-else{
-    alert("You can now drive")
-}
-console.log("Done")
+// if(a<0){
+//      alert("This is an invalid age")
+// }
+// else if(a<9){
+//      alert("You are a kid and you cannot even think of driving")
+// }
+// else if(a<18 && a>=9){
+//      alert("You are a kid and you can think of driving after 18")
+// }
+// else{
+//     alert("You can now drive")
+// }
+// console.log("Done")
+
+console.log("You can",(a<18? "not drive" : "drive"))
